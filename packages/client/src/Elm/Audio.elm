caf752a383f66f6f23a4f932544d7fd06891a3ce
M.D. van Es <es@mdworld.nl>
2020-04-11 15:23:07 +0200

    Switch station

--- packages/client/src/Elm/Audio.elm
+++ packages/client/src/Elm/Audio.elm
@@ -1,11 +1,12 @@
 module Elm.Audio exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text, audio)
-import Html.Attributes exposing (class, controls, type_, src, title)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, text, audio, select, option)
+import Html.Attributes exposing (class, controls, type_, src, title, value)
+import Html.Events exposing (onClick, on, targetValue)
+import Json.Decode as Json
 
---TODO cache busting, switch stations, get now playing
+--TODO cache busting, switch stations, get now playing, remove count/+1/-1 examples
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -13,25 +14,33 @@ main =
 
 -- MODEL
 
-type alias Model = Int
+type alias Model = { count: Int, channel: String }
 
 init : Model
 init =
-  0
+  { count = 0
+  , channel = "NPO Radio 2" }
 
+channelOptions = ["NPO Radio 2", "Sky Radio"]
+
+channelOption channel =
+    option [ value channel] [text channel]
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | GetNowPlaying | SetChannel String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
-
+      { model | count = model.count + 1 }
     Decrement ->
-      model - 1
+      { model | count = model.count - 1 }
+    GetNowPlaying ->
+      { model | count = model.count - 1 }
+    SetChannel val ->
+      { model | channel = val }
 
 
 -- VIEW
@@ -41,13 +50,15 @@ view model =
   div
     []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , div [] [ text (String.fromInt model.count) ]
     , button [ onClick Increment ] [ text "+" ]
     , div
         [ class "card" ]
-        [ div [] [ text "NYI Now Playing" ]
+        [ select [ on "change" (Json.map SetChannel targetValue)] (List.map channelOption channelOptions)
+        , div [] [ text "NYI Now Playing" ]
         , div [] [ text "NYI Programme" ]
-        , button [ onClick Increment , title "Refresh ~ do this onclick on logo" ] [ text "R" ]
+        , div [] [ text model.channel ]
+        , button [ onClick GetNowPlaying , title "Refresh ~ do this onclick on logo" ] [ text "R" ]
         , audio
             [ src "https://icecast.omroep.nl/radio2-bb-mp3"
             , type_ "audio/mpeg"
