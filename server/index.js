f2487620d5b7bd5df5191025ec59b0eec3ec3bcb
M.D. van Es <es@mdworld.nl>
2020-04-11 09:12:49 +0200

    Export library function

--- server/index.js
+++ server/index.js
@@ -5,20 +5,34 @@ const got = require('got');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/api/nowplaying/radio2', async (req, res) => {
-    try {
-      const nowonairResponse = await got('https://radiobox2.omroep.nl/data/radiobox2/nowonair/2.json').json();
-      const { artist, title, last_updated } = nowonairResponse.results[0].songfile;
-      // console.log(response.results[0].songfile);
-      const broadcastResponse = await got('https://radiobox2.omroep.nl/data/radiobox2/currentbroadcast/2.json').json();
-      const { name } = broadcastResponse.results[0];
-      // console.log(broadcastResponse.results[0].name);
-      res.send({ artist, title, last_updated, name });
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
-    }
-  }
-);
+// Export for use by other apps
+const getNowPlaying = async () => {
+  const nowonairResponse = await got('https://radiobox2.omroep.nl/data/radiobox2/nowonair/2.json').json();
+  const { artist, title, last_updated } = nowonairResponse.results[0].songfile;
+  const broadcastResponse = await got('https://radiobox2.omroep.nl/data/radiobox2/currentbroadcast/2.json').json();
+  const { name } = broadcastResponse.results[0];
+  return { artist, title, last_updated, name };
+}
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+const startServer = () => {
+  app.get('/', (req, res) => res.sendStatus(404));
+
+  app.get('/api/nowplaying/radio2', async (req, res) => {
+      try {
+        const response = await getNowPlaying();
+        res.send(response);
+      } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    }
+  );
+
+  app.listen(port, () => console.log(`App listening on port ${port}!`));
+}
+
+startServer();
+
+module.exports = {
+  getNowPlaying
+};
