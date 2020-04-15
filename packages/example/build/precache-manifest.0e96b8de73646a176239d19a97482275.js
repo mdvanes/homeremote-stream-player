74ab38b88b0b79bdc9db67ec1070e7e5be14734f
M.D. van Es <es@mdworld.nl>
2020-04-15 19:32:22 +0200

    Add deliverables

---
+++ packages/example/build/precache-manifest.0e96b8de73646a176239d19a97482275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16a7ab5ac73103c11d8e2eef89526da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fccd810422ba036f90f",
+    "url": "/static/css/main.1b6979cb.chunk.css"
+  },
+  {
+    "revision": "672a61cd8be73160fbd8",
+    "url": "/static/js/2.d72ac4a1.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/static/js/2.d72ac4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fccd810422ba036f90f",
+    "url": "/static/js/main.28040f32.chunk.js"
+  },
+  {
+    "revision": "7e65d857e43f4173d1e8",
+    "url": "/static/js/runtime-main.7813ffa7.js"
+  }
+]);
