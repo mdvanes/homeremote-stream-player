233cfa57539aa43535e9dc8f02e5235291b640f7
M.D. van Es <es@mdworld.nl>
2020-04-15 15:28:30 +0200

    Use React component

--- packages/example/src/App.js
+++ packages/example/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomeremoteStreamPlayer from '@mdworld/homeremote-stream-player';
+
+// TODO example app that uses these as dependencies
+//  add concurrently and example server
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeremoteStreamPlayer />
     </div>
   );
 }
