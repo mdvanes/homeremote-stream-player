70bfb1828b42ecdfa5a9f80259780452beec624f
M.D. van Es <es@mdworld.nl>
2020-04-25 14:46:48 +0200

    Build

--- packages/client/webpack.config.prod.js
+++ packages/client/webpack.config.prod.js
@@ -26,7 +26,7 @@ module.exports = {
         }
       },
       { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.svg$/, loader: "file-loader" },
+      { test: /\.svg$/, loader: "url-loader" },
       // loader for elm files
       elmloader
     ]
--- packages/example/build/service-worker.js
+++ packages/example/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e96b8de73646a176239d19a97482275.js"
+  "/precache-manifest.f2cab594d778f2c12df85ebdf2d3acdb.js"
 );
 
 self.addEventListener('message', (event) => {
