6e6e74fb9a6c0c733b3a2306b583222a85e9f6d2
M.D. van Es <es@mdworld.nl>
2020-09-08 19:37:37 +0200

    build

---
+++ packages/example/build/precache-manifest.fa8d9ad47ffd83657f64b43c32a29cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89dc7877c85bd7f1cd86cdb04460a83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fccd810422ba036f90f",
+    "url": "/static/css/main.1b6979cb.chunk.css"
+  },
+  {
+    "revision": "136e2bf7fc76f5c3f930",
+    "url": "/static/js/2.ea9a7218.chunk.js"
+  },
+  {
+    "revision": "3981f0049e963fa39a9de59f03263293",
+    "url": "/static/js/2.ea9a7218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fccd810422ba036f90f",
+    "url": "/static/js/main.28040f32.chunk.js"
+  },
+  {
+    "revision": "7e65d857e43f4173d1e8",
+    "url": "/static/js/runtime-main.7813ffa7.js"
+  }
+]);
