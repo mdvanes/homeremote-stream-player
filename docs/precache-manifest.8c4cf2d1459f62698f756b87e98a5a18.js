d5b35a35e135bf205d27d740a068cf0d0e3d385a
M.D. van Es <es@mdworld.nl>
2020-10-24 14:03:47 +0200

    build to docs

---
+++ docs/precache-manifest.8c4cf2d1459f62698f756b87e98a5a18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c54a3cdaeb5ea2b0b1336a905f2a6c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5757b0e016d93e19acb1.bundle.js"
+  },
+  {
+    "url": "runtime~main.5757b0e016d93e19acb1.bundle.js"
+  },
+  {
+    "revision": "7d60745eea65c115c106",
+    "url": "static/css/main.c440fb6b.chunk.css"
+  },
+  {
+    "revision": "bddd0db36e1c27ada1c949c1a35eeef7",
+    "url": "static/media/pause.bddd0db3.svg"
+  },
+  {
+    "revision": "c1e2bd208de33acb3c25a6a499aeb4db",
+    "url": "static/media/play.c1e2bd20.svg"
+  },
+  {
+    "url": "vendors~main.5757b0e016d93e19acb1.bundle.js"
+  },
+  {
+    "url": "vendors~main.5757b0e016d93e19acb1.bundle.js.LICENSE.txt"
+  }
+]);
