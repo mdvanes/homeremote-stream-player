b98943ff6ea481c304e3a3773c48f3b5349eae84
M.D. van Es <es@mdworld.nl>
2020-10-25 09:44:04 +0100

    Reformat for eslint

--- packages/streamplayer-client/src/HomeremoteStreamPlayer.stories.tsx
+++ packages/streamplayer-client/src/HomeremoteStreamPlayer.stories.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { FC, ReactNode } from "react";
 import HomeremoteStreamPlayer from "./HomeremoteStreamPlayer";
 import "./storybookStyles.css";
+import packageJson from "../package.json";
 
 export default {
-  title: "Elm/StreamPlayer",
-  component: HomeremoteStreamPlayer,
+    title: `Elm/StreamPlayer v${packageJson.version}`,
+    component: HomeremoteStreamPlayer,
 };
 
-export const Default = () => {
-  const url =
-    process.env.STORYBOOK_MODE === "PROD"
-      ? `https://${window.location.host}/${window.top.location.pathname}`
-      : "http://localhost:3100";
-  return <HomeremoteStreamPlayer url={url} />;
+const Wrapper: FC = ({ children }) => {
+    return (
+        <div
+            style={{
+                border: "1px solid red;",
+                minWidth: "500px",
+                maxWidth: "800px",
+            }}
+        >
+            {children}
+        </div>
+    );
+};
+
+export const Default = (): ReactNode => {
+    const url =
+        process.env.STORYBOOK_MODE === "PROD"
+            ? `https://${window.location.host}/${window.top.location.pathname}`
+            : "http://localhost:3100";
+    return (
+        <Wrapper>
+            <HomeremoteStreamPlayer url={url} />
+        </Wrapper>
+    );
 };
--- storybookMockServer.ts
+++ storybookMockServer.ts
@@ -1,55 +1,67 @@
-const express = require('express');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
 const app = express();
-const { getNowPlaying, ChannelName } = require('./packages/streamplayer-server/index');
+const {
+    getNowPlaying,
+    ChannelName,
+} = require("./packages/streamplayer-server/index");
+
 const port = 3100;
-
-const startServer = (corsMode: CORS_MODE) => {
-    app.get('/', (req, res) => res.sendStatus(404));
-
-    app.get('/api/nowplaying/radio2', async (req, res) => {
-            if(corsMode === CORS_MODE.DEBUG) {
-                console.log('CORS DEBUG MODE');
-                res.header("Access-Control-Allow-Origin", "http://localhost:6006"); // update to match the domain you will make the request from
-                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            }
-            try {
-                const response = await getNowPlaying(ChannelName.RADIO2);
-                res.send(response);
-            } catch (error) {
-                console.log(error);
-                res.sendStatus(500);
-            }
-        }
-    );
-
-    app.get('/api/nowplaying/radio3', async (req, res) => {
-            if(corsMode === CORS_MODE.DEBUG) {
-                console.log('CORS DEBUG MODE');
-                res.header("Access-Control-Allow-Origin", "http://localhost:6006"); // update to match the domain you will make the request from
-                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            }
-            try {
-                const response = await getNowPlaying(ChannelName.RADIO3);
-                res.send(response);
-            } catch (error) {
-                console.log(error);
-                res.sendStatus(500);
-            }
-        }
-    );
-
-    app.listen(port, () => console.log(`App listening on port ${port}!`));
-}
 
 enum CORS_MODE {
     NONE,
-    DEBUG
+    DEBUG,
 }
 
-const corsMode: CORS_MODE = process.argv.length > 2 && process.argv[2] === '--CORS=debug' ? CORS_MODE.DEBUG : CORS_MODE.NONE;
+const corsMode: CORS_MODE =
+    process.argv.length > 2 && process.argv[2] === "--CORS=debug"
+        ? CORS_MODE.DEBUG
+        : CORS_MODE.NONE;
+
+const startServer = (corsMode: CORS_MODE): void => {
+    app.get("/", (req, res) => res.sendStatus(404));
+
+    app.get("/api/nowplaying/radio2", async (req, res) => {
+        if (corsMode === CORS_MODE.DEBUG) {
+            console.log("CORS DEBUG MODE");
+            res.header("Access-Control-Allow-Origin", "http://localhost:6006"); // update to match the domain you will make the request from
+            res.header(
+                "Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept"
+            );
+        }
+        try {
+            const response = await getNowPlaying(ChannelName.RADIO2);
+            res.send(response);
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+    });
+
+    app.get("/api/nowplaying/radio3", async (req, res) => {
+        if (corsMode === CORS_MODE.DEBUG) {
+            console.log("CORS DEBUG MODE");
+            res.header("Access-Control-Allow-Origin", "http://localhost:6006"); // update to match the domain you will make the request from
+            res.header(
+                "Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept"
+            );
+        }
+        try {
+            const response = await getNowPlaying(ChannelName.RADIO3);
+            res.send(response);
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+    });
+
+    app.listen(port, () => console.log(`App listening on port ${port}!`));
+};
 
 startServer(corsMode);
 
 module.exports = {
-    getNowPlaying
+    getNowPlaying,
 };
