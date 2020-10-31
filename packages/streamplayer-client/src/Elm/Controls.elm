0d7ad7ad3f2217a93bed3a0943fc9ef8c2e70400
M.D. van Es <es@mdworld.nl>
2020-10-31 18:12:33 +0100

    Add inline svg icon

--- packages/streamplayer-client/src/Elm/Controls.elm
+++ packages/streamplayer-client/src/Elm/Controls.elm
@@ -4,7 +4,9 @@ import Debug exposing (log, toString)
 import Elm.Ports exposing (setPlayPauseStatusPort)
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
-import Html.Attributes exposing (class, classList)
+import Html.Attributes
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 type alias Model =
     { currentStatus : String }
@@ -37,16 +39,36 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div
-        [ class "controls" ]
+        [ Html.Attributes.class "controls" ]
         [ button
-            [ classList 
+            [ Html.Attributes.classList 
                 [ ("play", True)
                 , ("active", model.currentStatus == "Play")
                 ]
             , onClick (SetPlayPauseStatus Play) ]
-            []
+            [ svg
+                [ width "100"
+                , height "100"
+                , viewBox "10 9 30 30"
+                ]
+                [ Svg.path
+                    [ d "M-838-2232H562v3600H-838z"
+                    , fill "none"
+                    ]
+                    []
+                , Svg.path
+                    [ d "M16 10v28l22-14z"
+                    ]
+                    []
+                , Svg.path
+                    [ d "M0 0h48v48H0z"
+                    , fill "none"
+                    ]
+                    []
+                ]
+            ]
         , button
-            [ class "pause"
+            [ Html.Attributes.class "pause"
             , onClick (SetPlayPauseStatus Pause) ]
             []
         ]
