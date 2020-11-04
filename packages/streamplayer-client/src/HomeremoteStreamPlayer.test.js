f803fc70395d85957f3d32c223bb6dd684647081
M.D. van Es <es@mdworld.nl>
2020-11-04 19:27:08 +0100

    Add unit test

--- packages/streamplayer-client/src/HomeremoteStreamPlayer.test.js
+++ packages/streamplayer-client/src/HomeremoteStreamPlayer.test.js
@@ -1,6 +1,68 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React, { Component } from "react";
 import { render } from "@testing-library/react";
-import HomeremoteStreamPlayer from "./HomeremoteStreamPlayer";
+// In this case use `require`, see below. import HomeremoteStreamPlayer from "./HomeremoteStreamPlayer";
+
+let mockSubscribe = () => {
+    /* no-op */
+};
+
+// Example of naive mock:
+// jest.mock("react-elm-components", () => "mock-react-elm-components");
+
+// Example of FC mock:
+// jest.mock("react-elm-components", () => (props) => {
+//     // Do not destructure `props.ports` to keep destinct from inner `ports` argument
+//     console.log("props.ports", props.ports);
+//     // ports();
+//     const foo = (fn) => {
+//         mockSubscribe = fn;
+//     };
+//     const ports = { setPlayPauseStatusPort: { subscribe: foo } };
+//     // triggerPorts =
+//     props.ports(ports);
+//     return <p data-testid="mock-react-elm-components" />;
+// });
+
+// const MockReactElmComponent = React.forwardRef((props, ref) => {
+//     // Do not destructure `props.ports` to keep destinct from inner `ports` argument
+//     console.log("props.ports", props.ports);
+//     // ports();
+//     const foo = (fn) => {
+//         mockSubscribe = fn;
+//     };
+//     const ports = { setPlayPauseStatusPort: { subscribe: foo } };
+//     // triggerPorts =
+//     props.ports(ports);
+//     return <p ref={ref} data-testid="mock-react-elm-components" />;
+// });
+
+const playSpy = jest.fn();
+const pauseSpy = jest.fn();
+
+window.HTMLMediaElement.prototype.play = playSpy;
+window.HTMLMediaElement.prototype.pause = pauseSpy;
+
+class MockReactElmComponent extends Component {
+    render() {
+        // Do not destructure `props.ports` to keep destinct from inner `ports` argument
+        const foo = (fn) => {
+            mockSubscribe = fn;
+        };
+        const ports = { setPlayPauseStatusPort: { subscribe: foo } };
+        this.props.ports(ports);
+        return (
+            <p data-testid="mock-react-elm-components">
+                <audio />
+            </p>
+        );
+    }
+}
+
+// With ref:
+jest.mock("react-elm-components", () => MockReactElmComponent);
+
+const HomeremoteStreamPlayer = require("./HomeremoteStreamPlayer").default;
 
 jest.mock("./HomeremoteStreamPlayer.css", () => "");
 jest.mock("./Elm/Audio.elm", () => ({
@@ -10,15 +72,55 @@ jest.mock("./Elm/Audio.elm", () => ({
         },
     },
 }));
-jest.mock("react-elm-components", () => "mock-react-elm-components");
 
 describe("StreamPlayer client", () => {
+    beforeEach(() => {
+        jest.useFakeTimers();
+        playSpy.mockReset();
+        pauseSpy.mockReset();
+    });
+
+    afterEach(() => {
+        jest.runOnlyPendingTimers();
+        jest.useRealTimers();
+    });
+
     it("renders the element", () => {
-        const { baseElement } = render(<HomeremoteStreamPlayer />);
-        expect(
-            baseElement
-                .querySelector("mock-react-elm-components")
-                .getAttribute("src")
-        ).toBe("mock-elm-audio");
+        const { getByTestId } = render(<HomeremoteStreamPlayer />);
+        expect(getByTestId("mock-react-elm-components")).toBeInTheDocument();
+    });
+
+    it("calls audioElem.play on new status", () => {
+        // const { baseElement, debug, getByTestId } = render(
+        //     <HomeremoteStreamPlayer />
+        // );
+        render(<HomeremoteStreamPlayer />);
+        // debug(baseElement.querySelector("p"));
+        // expect(baseElement.querySelector("p")).toBeInTheDocument();
+        // debug(getby);
+        // expect(
+        //     baseElement
+        //         .querySelector("mock-react-elm-components")
+        //         .getAttribute("src")
+        // ).toBe("mock-elm-audio");
+        // triggerPorts();
+
+        // playSpy.mockReset();
+        // mockSubscribe();
+        // expect(playSpy).toHaveBeenCalledWith();
+        expect(playSpy).not.toHaveBeenCalled();
+        mockSubscribe("Play");
+        jest.runAllTimers();
+        expect(playSpy).toHaveBeenCalledWith();
+        expect(pauseSpy).not.toHaveBeenCalled();
+    });
+
+    it("calls audioElem.pause on new status", () => {
+        render(<HomeremoteStreamPlayer />);
+        expect(pauseSpy).not.toHaveBeenCalled();
+        mockSubscribe("Pause");
+        jest.runAllTimers();
+        expect(pauseSpy).toHaveBeenCalledWith();
+        expect(playSpy).not.toHaveBeenCalled();
     });
 });
