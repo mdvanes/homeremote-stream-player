14f954cd7c50579a56e585404ab2c6d85367ab54
M.D. van Es <es@mdworld.nl>
2020-11-05 20:20:27 +0100

    Refactor

--- packages/streamplayer-server/src/StreamPlayerAPI.test.ts
+++ packages/streamplayer-server/src/StreamPlayerAPI.test.ts
@@ -1,34 +1,38 @@
 import { getNowPlaying, ChannelName } from "./StreamPlayerAPI";
 import * as GotModule from "got";
+import { CancelableRequest } from "got";
 
 jest.mock("got");
 
 const gotSpy = jest.spyOn(GotModule, "default");
 
 const SomeArtist = "Some Artist";
+const SomePresenters = "Some Presenters";
+
+const mockBaseResponse = {
+    artist: SomeArtist,
+};
+
+const createMockRequest = (
+    additional: Record<string, string>
+): CancelableRequest<unknown> => {
+    const jsonResponse = {
+        data: [{ ...mockBaseResponse, ...additional }],
+    };
+    return {
+        json: () => Promise.resolve(jsonResponse),
+    } as CancelableRequest<unknown>;
+};
 
 describe("StreamPlayerAPI", () => {
     beforeEach(() => {
         gotSpy.mockReset();
-        const jsonResponse = {
-            data: [{ artist: SomeArtist, presenters: "SomePresenters" }],
-        };
-        gotSpy.mockReturnValue({
-            json: () => jsonResponse,
-        } as any);
+        gotSpy.mockReturnValue(
+            createMockRequest({ presenters: SomePresenters })
+        );
     });
 
     it("reponds with transformed message", async () => {
-        // const y: Partial<Request> = {
-        //     json: (): Promise<any> => ({
-        //         data: [{ artist: SomeArtist }],
-        //     }),
-        // };
-        // gotSpy.mockReturnValue({
-        //     json: () => ({
-        //         data: [{ artist: SomeArtist }],
-        //     }),
-        // } as any);
         const response = await getNowPlaying(ChannelName.RADIO2);
         expect(gotSpy).toHaveBeenCalledWith(
             "https://www.nporadio2.nl/api/tracks"
@@ -39,17 +43,13 @@ describe("StreamPlayerAPI", () => {
             // eslint-disable-next-line @typescript-eslint/camelcase
             last_updated: undefined,
             songImageUrl: "",
-            name: "undefined / SomePresenters",
+            name: `undefined / ${SomePresenters}`,
             imageUrl: "",
         });
     });
 
     it("can respond when presenters not defined", async () => {
-        gotSpy.mockReturnValue({
-            json: () => ({
-                data: [{ artist: SomeArtist }],
-            }),
-        } as any);
+        gotSpy.mockReturnValue(createMockRequest({}));
         const response = await getNowPlaying(ChannelName.RADIO2);
         expect(gotSpy).toHaveBeenCalledWith(
             "https://www.nporadio2.nl/api/tracks"
@@ -71,11 +71,7 @@ describe("StreamPlayerAPI", () => {
     });
 
     it("can respond when presenters not defined for Radio 3", async () => {
-        gotSpy.mockReturnValue({
-            json: () => ({
-                data: [{ artist: SomeArtist }],
-            }),
-        } as any);
+        gotSpy.mockReturnValue(createMockRequest({}));
         const response = await getNowPlaying(ChannelName.RADIO3);
         expect(response).toEqual({
             artist: SomeArtist,
