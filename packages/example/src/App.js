c1d2c2111b1ad8e94663007e197ae812ea3b8456
M.D. van Es <es@mdworld.nl>
2021-05-22 20:10:12 +0200

    Commit build

--- packages/example/src/App.js
+++ packages/example/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import StreamPlayer from '@mdworld/homeremote-stream-player';
+import DockerList from '@mdworld/homeremote-dockerlist';
 
 function App() {
   return (
     <div className="App">
       <StreamPlayer url="http://localhost:3200" />
+      <DockerList url="http://localhost:3200" />
     </div>
   );
 }
